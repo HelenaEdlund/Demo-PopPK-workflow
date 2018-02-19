5aa914c9469c619f932d7441aa2cf93bfa5c5b25
Helena Edlund <helena.edlund@astrazeneca.com>
2018-02-19 16:07:54 -0500

    camel to snake

--- Scripts/s00_main.R
+++ Scripts/s00_main.R
@@ -30,7 +30,7 @@ check_session()
 
 
 # ----------- Read in source data and set stucture for R ------------------
-source(file = file.path(scripts_dir, "s01_datasetPrep.R"))
+source(file = file.path(scripts_dir, "s01_dataset_preparation.R"))
 
 
 # The .Rmd scripts below generate a .html file with the output. 
@@ -39,18 +39,18 @@ source(file = file.path(scripts_dir, "s01_datasetPrep.R"))
 
 # ----------- Dataset checkout ------------------
 # Go through the script to make sure all relevant columns are included and checked
-rmarkdown::render(input=file.path("Scripts","s02_datasetReview.Rmd"))
+rmarkdown::render(input=file.path("Scripts","s02_dataset_review.Rmd"))
 
 # ----------- Summarize excluded data ------------------
-rmarkdown::render(input=file.path("Scripts","s03_summaryOfExcludedData.Rmd"), 
-                  params = list(print_results=TRUE))
+rmarkdown::render(input=file.path("Scripts","s03_summary_excluded_data.Rmd"), 
+                  params = list(print_results=F))
 
 # ----------- Exploratory Data Analysis ------------------
-rmarkdown::render(input = file.path("Scripts", "s04_EDACovariates.Rmd"), 
+rmarkdown::render(input = file.path("Scripts", "s04_eda_covariates.Rmd"), 
                   params = list(print_results=F))
-rmarkdown::render(input = file.path("Scripts", "s05_EDAConcTime.Rmd"), 
+rmarkdown::render(input = file.path("Scripts", "s05_eda_conc_time.Rmd"), 
                   params = list(print_results=F))
-rmarkdown::render(input = file.path("Scripts", "s06_EDAPkLinearity.Rmd"), 
+rmarkdown::render(input = file.path("Scripts", "s06_eda_pk_linearity.Rmd"), 
                   params = list(print_results=F))
 
 # The error: 
@@ -60,8 +60,8 @@ rmarkdown::render(input = file.path("Scripts", "s06_EDAPkLinearity.Rmd"),
 # ----------- Preparation of NONMEM datasets ------------------
 # If printCSV is true the script outputs the dataset(s) to "DerivedData", 
 # otherwise it just saves the datasets and the names of the datasets s07.RData
-rmarkdown::render(input =file.path("Scripts","s07_NMDatasets.Rmd"), 
-                  params = list(printCSV=T))
+rmarkdown::render(input =file.path("Scripts","s07_nm_datasets.Rmd"), 
+                  params = list(print_csv=TRUE))
 
 
 
